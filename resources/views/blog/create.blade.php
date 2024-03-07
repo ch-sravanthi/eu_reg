5ec2be895ce6883a769467ae0dbead10644a6ae0
itsravanthi <ch.sravanthi@zastaindia.com>
2024-03-07 11:42:03 +0530

    col

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -82,8 +82,8 @@
 						</div>
 						
 						<div class="col-lg-6 mb-2">
-							<label>{{ $blog->nicenames['description'] }} </label>
-								{{ Form::textarea('description', old('description', $blog->description), ['rows' => 5,'placeholder' => 'Post Description'],['class' => 'form-control' ]) }}
+							<label>  </label>
+								{{ Form::textarea('description', old('description', $blog->description), ['rows' => 5,'placeholder' => 'Job Description'],['class' => 'form-control' ]) }}
 									@if($errors->has('description'))
 										<div class="text-danger">{{ $errors->first('description') }}</div>
 									@endif
