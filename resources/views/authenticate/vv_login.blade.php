44448307b695bebd9044f4c196c813711f506820
itsravanthi <ch.sravanthi@zastaindia.com>
2024-08-19 16:22:31 +0530

    alret u

--- resources/views/authenticate/vv_login.blade.php
+++ resources/views/authenticate/vv_login.blade.php
@@ -11,11 +11,16 @@
 							{!! Form::email('email', old('email', ''), ['class' => 'form-control', 'placeholder' => 'Enter VV Subscription email ID']) !!}
 							@if ($errors->has('email'))
 								<div class="text-danger">{{ $errors->first('email') }}</div>
+								<script>
+									document.addEventListener("DOMContentLoaded", function() {
+										myFunction("{{ $errors->first('email') }}");
+									});
+								</script>
 							@endif
 						</div>
 						
 						<div class="pb-2">
-							<button class="btn btn-theme mr-3 w-100">Login</button>
+							<button class="btn btn-theme mr-3 w-100"  onclick="return validateForm()">Login</button>
 								<br/>
 								<p style="text-align:center;font-size:15px; line-height:1.2">
 									<br>"email ID Doesn't Match Please Contact - 7702471313"
@@ -26,4 +31,28 @@
 		</div>
 		{!! Form::close() !!}
 	</div>
+	
+	<script>
+		function myFunction(errorMessage) {
+			if (errorMessage) {
+				alert("Error: " + errorMessage);
+			}
+		}
+
+		function validateForm() {
+			var email = document.querySelector("input[name='email']").value;
+			
+			if (!email) {
+				alert("Email field cannot be empty.");
+				return false; // Prevent form submission
+			}
+			if (!$user) { 
+				alert("Please Subscribe VV Softcopy");
+				return false; 
+			}
+			
+			return true;
+		}
+	</script>
+
 @endsection
